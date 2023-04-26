c9f82f8768e7c9aa43d81fe5b79dde0eed9bbaf4
Matt Hammond <mjhammond94@gmail.com>
2023-04-26 09:32:10 +0100

    spec test added - code cleaned

--- spec/books_repository_spec.rb
+++ spec/books_repository_spec.rb
@@ -20,40 +20,35 @@ describe BooksRepository do
     expect(list.first.author_name).to eq 'Hunter.S Thompson'
     expect(list.first.id).to eq "1"
   end
-end
 
-describe BooksRepository do
-  def reset_books_table
-    seed_sql = File.read('spec/seeds_bookstore.sql')
-    connection = PG.connect({ host: '127.0.0.1', dbname: 'bookstore_test' })
-    connection.exec(seed_sql)
-  end
-
-  before(:each) do
-    reset_books_table
-  end
-
-  it 'Should return the title and author of book at id given to find as argument' do
+  it 'Should return the title of the book at id given to find method as argument' do
     repo = BooksRepository.new
     book = repo.find(1)
     expect(book.title).to eq 'Fear and Loathing in Las Vegas'
   end
+
+  it 'Should return the name of the author at id given to find method as argument' do
+    repo = BooksRepository.new
+    book = repo.find(4)
+    expect(book.author_name).to eq 'Lewis Carrol'
+  end
 end
 
 describe BooksRepository do
+  context 'Empty Databases' do
+    def reset_albums_table
+      seed_sql = File.read('spec/seeds_empty.sql')
+      connection = PG.connect({ host: '127.0.0.1', dbname: 'bookstore_test' })
+      connection.exec(seed_sql)
+    end
 
-  def reset_albums_table
-    seed_sql = File.read('spec/seeds_empty.sql')
-    connection = PG.connect({ host: '127.0.0.1', dbname: 'bookstore_test' })
-    connection.exec(seed_sql)
-  end
-
-  before(:each) do
-    reset_albums_table
-  end
-  
-  it 'Should return an empty array when DB is empty' do
-    repo = BooksRepository.new
-    expect(repo.all).to eq []
+    before(:each) do
+      reset_albums_table
+    end
+    
+    it 'Should return an empty array when DB is empty' do
+      repo = BooksRepository.new
+      expect(repo.all).to eq []
+    end
   end
 end
